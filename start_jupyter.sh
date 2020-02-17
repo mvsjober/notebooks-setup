a7bd6b32db5a7651f8a3207abf6e1b65c0709527
Mats Sjöberg <mats.sjoberg@csc.fi>
2020-02-17 14:54:33 +0000

    Updated run script

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate notebooks
+conda activate notebooks-2020-02
 jupyter-lab --ip 0.0.0.0 \
             --certfile=~/.jupyter/mycert.pem --keyfile=~/.jupyter/mykey.key \
             --notebook-dir=lab/
