c5670d1580d3f66aa8fc8f7ac92b696dea8c059c
Mats Sjöberg <mats.sjoberg@csc.fi>
2020-05-27 17:07:40 +0300

    Recent changes

--- start_jupyter_local.sh
+++ start_jupyter_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/sw/opt/miniconda3/etc/profile.d/conda.sh
-conda activate notebooks-cpu-2020-02
-jupyter-lab --notebook-dir=lab/
+conda activate notebooks
+jupyter-lab --notebook-dir=~/code/
